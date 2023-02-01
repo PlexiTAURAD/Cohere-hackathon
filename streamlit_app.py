d2a51987c7fd750982dc823e9a6384de45fbf878
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2023-02-01 19:03:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,4 @@ _ italics _
 st.latex(r''' a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)''')
-d ={
-    "name":"Harsh",
-    "language":"Python",
-    "topic":"Streamlit"
-} 
-st.write(d)
+
