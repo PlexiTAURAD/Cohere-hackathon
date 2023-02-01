d3eb1458ef6af892ea2aa6e74e07b1e7cf704a72
rthmk45 <123261170+rthmk45@users.noreply.github.com>
2023-02-02 01:14:05 +0530

    Update streamlit_2.py

--- streamlit_2.py
+++ streamlit_2.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-a = [1,2,3,4,5,6,7,8]
-n = np.array(a)
+A = [1,2,3,4,5,6,7,8]
+n = np.array(A)
 nd = n.reshape((2,4))
 
-
-st.table(a)
+st.table(A)
