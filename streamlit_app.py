4c12ab3b28045eb27d846c3b512a87e1855cd5ea
rthmk45 <123261170+rthmk45@users.noreply.github.com>
2023-02-02 01:51:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as pl
 
 data = pd.DataFrame(
      np.random.randn(100,3),
@@ -15,3 +16,8 @@ data = pd.DataFrame(
 st.line_chart(data)
 st.area_chart(data)
 st.bar_chart(data)
+
+pl.scatter(data['a'],data['b'])
+pl.title('blah')
+st.pyplot()
+
