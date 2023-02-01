032d78ae840e8e5c7e4dfe017484b2d75db8644d
rthmk45 <123261170+rthmk45@users.noreply.github.com>
2023-02-02 01:14:58 +0530

    Update streamlit_2.py

--- streamlit_2.py
+++ streamlit_2.py
@@ -1,9 +1,25 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-A = [1,2,3,4,5,6,7,8]
-n = np.array(A)
-nd = n.reshape((2,4))
+st.title("your mom")
 
-st.table(A)
+st.header("is")
+
+st.subheader("gay")
+
+st.text('bcoz she gave birth to a pussy wussy')
+st.markdown('''# GAY
+## LOL
+### LMAO
+:moon: :sunglasses: <br>
+** lololollolol**
+_hehehehehe_
+''',True)
+
+d = {
+"dvffvd":"frgrgeg", "dvdvfd":"fdffdfdd",
+  "devfdvfdv":"fdfdfvgfdvfdb"
+
+
+
+}
+st.write(d)
