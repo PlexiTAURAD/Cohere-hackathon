7f66f0e4f0b8fce5f766a5dd2e86a257c9884557
rthmk45 <123261170+rthmk45@users.noreply.github.com>
2023-02-01 19:52:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,16 @@ st.title("your mom")
 st.header("is")
 
 st.subheader("gay")
-
+#efefedf
 st.text('bcoz she gave birth to a pussy wussy')
 st.markdown('''# GAY
 ## LOL
 ### LMAO
 :moon: :sunglasses: <br>
 ** lololollolol**
-_heheheheh_
+_hehehehehe_
 
 ''',True)
+
+
+
