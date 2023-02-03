b1493ae4610be336cda2507bb4dbebdd565f594d
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2023-02-03 15:04:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,20 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import altair as alt
+import webbrowser
 
-data = pd.DataFrame(
-     np.random.randn(100,3),
-  
-  columns = ['a','b','c']
+st.title("ODR AI ASSISTANTS")
 
+input_text = st.text_input("Enter your text:")
+output = 0
 
-)
+if input_text:
+    # Perform processing on input_text here to get the output
+    output = 1
 
-st.line_chart(data)
-st.area_chart(data)
-st.bar_chart(data)
-chart = alt.Chart(data).mark_circle().encode(
-x = 'a',y= 'b'
+if output == 0:
+    additional_input = st.text_input("Additional Input:")
 
+if output == 1:
+    st.write("Output:", output)
 
-)
-st.graphviz_chart("""
-digraph{
-watch -> like
-like -> share
-share -> subscribe
-share -> watch
-}
-""")
-
-data1 = pd.DataFrame({
-
-'T1' : ['Chic'],
-     
-
-     'lat' : [13.124042],
-     'lon' : [77.590924]
-
-
-
-
-
-
-
-
-
-})
-
-if st.button("bleh"):
-      st.map(data1)
-      
-      
-      
-name = st.text_input('Enter your name')
-st.write('your name is ',name)
-      
-address = st.text_area('Enter your address')
-st.write('your address is ',address)
-
-
-
-st.date_input('enter the date')
-st.time_input('enter the time')
- 
-A = st.checkbox('testing')
-if A == True :
-      st.write('yes')
-
-
-#Testing git push remote
+if st.button("Analytics"):
+    webbrowser.open_new_tab("https://www.example.com/bar-graph")
