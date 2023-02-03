3b2431b3ec069286f0dc5e46d8b5b4e755cadda9
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2023-02-04 01:18:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 
+exec(open("model.ipynb").read())
+
 st.title("ODR AI ASSISTANTS")
 
-first_input = st.text_input("Enter your text:")
+first_input = st.text_input("Enter your Query: ")
 
-if first_input:
-    result = "Output"
-    output = st.write(result)
-
-    if output == 0:
-        st.write("Please provide additional information:")
-        second_input = st.text_input("Additional Information:")
-
-if first_input or second_input:
-    st.write("Analysis button")
-    if st.button("Analytics"):
-        st.write("Displaying bar graph")
+if first_input == '0' or '1':
+     st.write(gen)
+elif first_input == '5':
+     warranty_input = st.text_input("Do you have warranty?")
+     if warranty == 0:
+        st.write("That's okay. You can visit our stores or connect with us at repairs@lenuwu.com to see how much it will cost you")
+     if warranty == 1:
+        st.write("Alright, you can come to our store or get a repairman to visit you and get your device repaired free of cost")
+elif first_input == '6':
+    if feelings == 8:
+        st.write(feel)
+    else:
+        st.write(feel)
